1a6b7a846cbcab9dd66cd8793b29cbe7f42dec59
Arshdeep singh <arshgill6120@gmail.com>
2025-12-25 18:55:48 +0530

    basic frontend of the todo app

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import { TodoCard } from "./TodoCard";
+
+export function TodoList(props) {
+  const {todos} = props
+
+
+  return <>
+    {todos.map((todo, todoIndex)=>{
+      return(
+        <TodoCard key={todoIndex} todoIndex={todoIndex} {...props}/>
+      )
+    })}
+  </>
+}
