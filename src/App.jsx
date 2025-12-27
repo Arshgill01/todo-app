fe802f6d2af991d1280780898ac0aba99a621202
Arshdeep singh <arshgill6120@gmail.com>
2025-12-27 21:44:48 +0530

    Started with functional components, and the stateful variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,34 @@ import { Tabs } from "./components/Tabs";
 import { Header } from "./components/Header";
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
+import { useState } from "react";
 function App(){
 
-  const todos = [
-  { input: 'Hello! Add your first todo!', complete: true },
-  { input: 'Get the groceries!', complete: false },
-  { input: 'Learn how to web design', complete: false },
-  { input: 'Say hi to gran gran', complete: true },
-  ]
+  // const todos = [
+  // { input: 'Hello! Add your first todo!', complete: true },
+  // { input: 'Get the groceries!', complete: false },
+  // { input: 'Learn how to web design', complete: false },
+  // { input: 'Say hi to gran gran', complete: true },
+  // ]
+
+  const [todos, setTodos] = useState([{ input: 'Hello! Add your first todo!', complete: true }]);
+
+  function handleAddTodo(newTodo){
+    const newTodoList = [...todos, {input: newTodo, complete: false}];
+    setTodos(newTodoList);
+   
+  }
+
+ // spreading out the todos -- currently one one default todo, 
+    // and then creating a newtodo, which is the parameter, and the value for the complete status, which would be false in the start. 
+
   return(
     <>
 
 
     <Header todos={todos}/>
     <Tabs todos={todos}/>
-    <TodoInput todos={todos}/>
+    <TodoInput handleAddTodo={handleAddTodo}/>
     <TodoList todos={todos}/>
 
 
